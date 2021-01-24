0e6f45af369c2185a13d0da1171b0ad24397238a
Nikhilkumar8143 <63304207+Nikhilkumar8143@users.noreply.github.com>
2021-01-24 18:53:09 +0530

    changed href values

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import downloadappstore from '../assets/img/downloadappstore.jpg';
 import downloadgoogleplay from '../assets/img/downloadgoogleplay.jpg';
